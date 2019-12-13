0f93a0d991db51549770932d8937f8e6a4193b52
Long Vu <vu.long@ouranos.ca>
2019-12-13 15:30:26 -0500

    add backup scripts

---
+++ scripts/backup-datavolume.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+# Backup DATA_VOL_NAME to BACKUP_OUT_DIR/DATA_VOL_NAME.tgz
+
+DATA_VOL_NAME="$1"; shift
+BACKUP_OUT_DIR="$1"; shift
+
+echo "Will create $BACKUP_OUT_DIR/$DATA_VOL_NAME.tgz"
+
+docker run --rm \
+  --name backup_data_vol_$DATA_VOL_NAME \
+  -u root \
+  -v $BACKUP_OUT_DIR:/backups \
+  -v $DATA_VOL_NAME:/data_vol_to_backup:ro \
+  bash \
+  tar czf /backups/$DATA_VOL_NAME.tgz -C /data_vol_to_backup .
+
+# vi: tabstop=8 expandtab shiftwidth=4 softtabstop=4
