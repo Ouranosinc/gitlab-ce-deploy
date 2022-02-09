79b98dbbe22e823bcbee965129d7ed98d56f13fc
Long Vu <vu.long@ouranos.ca>
2022-02-09 17:02:09 -0500

    vagrant: provision self-signed Wildcard SSL certificate

--- vagrant-provisioning/provision.sh
+++ vagrant-provisioning/provision.sh
@@ -2,6 +2,17 @@
 
 cd /vagrant
 
+if [ -f ./env.local ]; then
+    # Get SSL_CERTIFICATE, SSL_CERTIFICATE_KEY from existing env.local.
+    . ./env.local
+fi
+
+if [ ! -f "$SSL_CERTIFICATE" -a ! -f "$SSL_CERTIFICATE_KEY" ]; then
+    # If both have bogus value, set appropriate one for Vagrant.
+    SSL_CERTIFICATE="/home/vagrant/cert.pem"
+    SSL_CERTIFICATE_KEY="/home/vagrant/key.pem"
+fi
+
 if [ ! -f env.local ]; then
     cp env.local.example env.local
     cat <<EOF >> env.local
@@ -9,8 +20,22 @@ if [ ! -f env.local ]; then
 # override with values needed for vagrant
 export HOSTNAME_FQDN='${VM_HOSTNAME}.$VM_DOMAIN'
 # Refine because GITLAB_WEB_URL depends on HOSTNAME_FQDN.
-export GITLAB_WEB_URL='http://${HOSTNAME_FQDN}'
+export GITLAB_WEB_URL='https://${HOSTNAME_FQDN}'
+
+export SSL_CERTIFICATE="$SSL_CERTIFICATE"
+export SSL_CERTIFICATE_KEY="$SSL_CERTIFICATE_KEY"
+
 EOF
 else
     echo "existing env.local file, not overriding"
+fi
+
+if [ ! -f "$SSL_CERTIFICATE" -a ! -f "$SSL_CERTIFICATE_KEY" ]; then
+    . ./env.local
+    openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 \
+        -keyout "$SSL_CERTIFICATE_KEY" \
+        -out "$SSL_CERTIFICATE" \
+	-subj "/C=CA/ST=Quebec/L=Montreal/O=RnD/CN=*.${VM_HOSTNAME}.$VM_DOMAIN"
+else
+    echo "existing '$SSL_CERTIFICATE' or '$SSL_CERTIFICATE_KEY' file, not overriding"
 fi
